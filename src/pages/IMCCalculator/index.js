1fb8481d80adbfc1c53e780cb8d7a9256ce6d6b1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-06-22 07:44:41 -0300

    Estrutura inicial e Router

---
+++ src/pages/IMCCalculator/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class IMCCalculator extends Component {
+  render () {
+    return (
+      <h1>This is IMC Calculator</h1>
+    );
+  }
+}
