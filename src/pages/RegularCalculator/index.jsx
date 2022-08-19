0d29e01ed3503736cff6823384223d9565a61f00
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-19 10:13:52 -0300

    Removing parentesis from display validation

--- src/pages/RegularCalculator/index.jsx
+++ src/pages/RegularCalculator/index.jsx
@@ -1,93 +1,106 @@
-import React from 'react';
-import { useState } from 'react';
-import { simplifyExpression } from './calculatorLogic';
-import Button from '../../components/Button';
-import Display from '../../components/Display';
-import './index.css';
+import React from "react";
+import { useState } from "react";
+import { simplifyExpression } from "./calculatorLogic";
+import Button from "../../components/Button";
+import Display from "../../components/Display";
+import "./index.css";
 
 const RegularCalculator = () => {
-  const generics = ['AC', '(', ')', '7', '8', '9', '4', '5', '6', '1', '2', '3', '←', '0', '.'];
-  const operators = ['/', '*', '-', '+', '='];
+  const generics = [
+    "AC",
+    "(",
+    ")",
+    "7",
+    "8",
+    "9",
+    "4",
+    "5",
+    "6",
+    "1",
+    "2",
+    "3",
+    "←",
+    "0",
+    ".",
+  ];
+  const operators = ["/", "*", "-", "+", "="];
 
-  const [expression, setExpression] = useState('');
+  const [expression, setExpression] = useState("");
   const [result, setResult] = useState(0);
 
   const clearDisplay = () => {
-    setExpression('');
+    setExpression("");
     setResult(0);
   };
 
   const backSpace = () => {
-    setExpression(expression.substring(0, expression.length -1));
+    setExpression(expression.substring(0, expression.length - 1));
   };
 
   const actualizeDisplay = (newInput) => {
     setExpression((prev) => prev + newInput);
-    const lastInput = expression.substring(expression.length-1);
+    const lastInput = expression.substring(expression.length - 1);
 
-    const signs = ['(', ')', '/', '*', '-', '+', '.']
+    const signs = ["/", "*", "-", "+", "."];
     const isSign = (input) => signs.some((operator) => operator === input);
 
-    if(isSign(lastInput) && isSign(newInput)) {
+    if (isSign(lastInput) && isSign(newInput)) {
       setExpression(expression);
     }
   };
 
   const calculateExpression = () => {
     setResult(simplifyExpression(expression));
-  }
+  };
 
   const getInputs = ({ target: { innerText } }) => {
     switch (innerText) {
-      case 'AC':
+      case "AC":
         clearDisplay();
         break;
 
-      case '←':
+      case "←":
         backSpace();
         break;
 
-      case '=':
+      case "=":
         calculateExpression();
         break;
-    
+
       default:
         actualizeDisplay(innerText);
         break;
     }
-  }
+  };
 
   return (
     <main>
       <h1>Calculadora Regular</h1>
       <div className="calculator-container">
-        <Display
-          expression={ expression }
-          result={ result.toString() }
-        />
+        <Display expression={expression} result={result.toString()} />
         <div className="generics-wrapper">
           {generics.map((generic, i) => (
-            <Button 
-              key={ i }
-              label={ generic }
+            <Button
+              key={i}
+              label={generic}
               additionalClassName="number"
-              onClickCallback={ (e) => getInputs(e) }
+              onClickCallback={(e) => getInputs(e)}
             />
           ))}
         </div>
         <div className="operators-wrapper">
           {operators.map((operator, i) => (
-              <Button 
-                key={ i }
-                label={ operator }
-                additionalClassName="operator"
-                onClickCallback={ (e) => getInputs(e) }          
-              />
-            ))}
+            <Button
+              key={i}
+              label={operator}
+              additionalClassName="operator"
+              onClickCallback={(e) => getInputs(e)}
+            />
+          ))}
         </div>
       </div>
     </main>
   );
-}
+};
 
 export default RegularCalculator;
